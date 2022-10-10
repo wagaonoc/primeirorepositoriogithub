d92d2f21572ada00f72546ac76d6ca03d257eb64
wagaonoc <wagaonoc@gmail.com>
2022-10-09 22:52:57 -0300

    Primeira versão de codigo enviado para o github

---
+++ app.py
@@ -0,0 +1,7 @@
+# EXEMPLO DE CODIGO FEITO APENAS PARA TESTAR O ENVIO AO GITHUB
+
+N1 = int(input("Digite o primeiro numero: "))
+N2 = int(input("Digite o segundo numero: "))
+
+Soma = N1 + N2
+print (Soma)
